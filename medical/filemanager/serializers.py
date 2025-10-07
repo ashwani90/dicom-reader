ce3502f41e51ec39e0808a2b7428e56c4f0a8562
Ashwani <ashwani90125@gmail.com>
2025-10-08 00:51:29 +0530

    Create new api set

--- medical/filemanager/serializers.py
+++ medical/filemanager/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import File
+from .models import File, Case
 
 class FileSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,3 +8,15 @@ class FileSerializer(serializers.ModelSerializer):
             "id", "filename", "filetype", "path",
             "anonymized_path", "is_dicom", "modality", "uploaded_at"
         ]
+        
+class CaseSerializer(serializers.ModelSerializer):
+    files = FileSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Case
+        fields = [
+            "id", "case_id", "title", "description", "patient_name",
+            "patient_id", "patient_age", "patient_sex", "diagnosis",
+            "created_by", "created_at", "updated_at", "files"
+        ]
+        read_only_fields = ["created_by", "created_at", "updated_at"]
