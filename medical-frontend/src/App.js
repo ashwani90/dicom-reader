a3f8cfd5cb2aed9660be310513e45394cb1b6e47
Ashwani <ashwani90125@gmail.com>
2025-10-11 00:04:55 +0530

    Frontend setup

---
+++ medical-frontend/src/App.js
@@ -0,0 +1,59 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider } from "./auth/AuthContext";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import CaseList from "./pages/CaseList";
+import CaseDetail from "./pages/CaseDetail";
+import CaseForm from "./pages/CaseForm";
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Wrap protected routes */}
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Navbar />
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Navbar />
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/cases" element={<ProtectedRoute>
+                <Navbar />
+                <CaseList />
+              </ProtectedRoute>} />
+          <Route path="/cases/new" element={<ProtectedRoute>
+                <Navbar />
+                <CaseForm />
+              </ProtectedRoute>} />
+          <Route path="/cases/:id" element={<ProtectedRoute>
+                <Navbar />
+                <CaseDetail />
+              </ProtectedRoute>} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
