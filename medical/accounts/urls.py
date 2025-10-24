8888216cacb83d11b785cf6d00e34af9eb9c275b
Ashwani <ashwani90125@gmail.com>
2025-10-24 12:25:46 +0530

    Refactor api request

--- medical/accounts/urls.py
+++ medical/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, RefreshTokenView, LogoutView, UserView
+from .views import RegisterView, LoginView, RefreshTokenView, LogoutView, UserView, UserListView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('refresh/', RefreshTokenView.as_view(), name='token_refresh'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('user/', UserView.as_view(), name='user'),
+    path('users/', UserListView.as_view(), name='users'),
 ]
